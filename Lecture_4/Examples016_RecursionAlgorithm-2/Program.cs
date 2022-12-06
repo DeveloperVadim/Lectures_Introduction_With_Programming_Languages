95b2705f1bcdd3f78617a0c23dfee0cfc4d539ca
Вадим <118062456+DeveloperVadim@users.noreply.github.com>
2022-12-07 00:58:28 +0700

    Lecture 4

---
+++ Lecture_4/Examples016_RecursionAlgorithm-2/Program.cs
@@ -0,0 +1,9 @@
+﻿int[,] matrix=new int[3,4];
+for (int i = 0; i < matrix.GetLength(0); i++) // matrix.GetLength(0) -строки
+{
+    for (int j = 0; j < matrix.GetLength(1); j++) // matrix.GetLength(1) -столбцы
+    {
+        Console.Write($"{matrix[i,j]} ");
+    }
+     Console.WriteLine();
+}
---
+++ Lecture_4/Examples016_RecursionAlgorithm-3/Program.cs
@@ -0,0 +1,27 @@
+﻿void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++) // matrix.GetLength(0) -строки
+    {
+        for (int j = 0; j < matr.GetLength(1); j++) // matrix.GetLength(1) -столбцы
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(1, 10); //[1; 10)
+        }
+    }
+}
+
+int[,] matrix = new int[3, 4];
+PrintArray(matrix);
+FillArray(matrix);
+Console.WriteLine();
+PrintArray(matrix);
